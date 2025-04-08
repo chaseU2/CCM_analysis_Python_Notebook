0c0a17c1f0231fdfba632192a8f1de1e4f8830a4
Karl Balzer <130858385+chaseU2@users.noreply.github.com>
2025-04-08 05:16:56 +0200

    Update __init__.py

--- ccm_analysis_pynb/__init__.py
+++ ccm_analysis_pynb/__init__.py
@@ -1,4 +1,4 @@
 from .analysis import run_ccm_analysis_jupyter
 
-__version__ = "0.2.0"  # WICHTIG: Muss mit setup.py übereinstimmen
+__version__ = "1.1.0"  # WICHTIG: Muss mit setup.py übereinstimmen
 __all__ = ['run_ccm_analysis_jupyter']
